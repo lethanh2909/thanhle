895ff3ae8e2ff51c89e5fb6d6c687b2889321847
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-06 14:24:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,39 @@
     </head>
     <body>
     	<?php
-        echo 'hello';
+        	//$pdo = new PDO('pgsql:host=localhost;port=5432;dbname=GWCourses', 'postgres', '12345678');
+        	//echo "done!!!!!!";
+	    	$db = parse_url(getenv("DATABASE_URL"));
+
+			$pdo = new PDO("pgsql:" . sprintf(
+			    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+			    $db["host"],
+			    $db["port"],
+			    $db["user"],
+			    $db["pass"],
+			    ltrim($db["path"], "/")
+));
+
+        	$sql = "SELECT studentname, course FROM registercourse";
+        	$stmt = $pdo->prepare($sql);
+        	// thiet lap kieudu lieu tra ve
+        	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+        	$stmt->execute();
+        	$resultSet = $stmt->fetchAll();
+
+
         ?>
+        <ul>
+		<?php  
+			foreach ($resultSet as $row) {
+			echo '<li>' .
+				$row['studentname'] . ' --' . $row['course'] 
+				. '</li>';
+			}
+		?>
+		</ul>
     </body>
 </html>
+
+
+
