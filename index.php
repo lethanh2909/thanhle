d930470c6a12c52d08eb1980ece9c08ce6ae66b3
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-06 14:32:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,17 @@
     	<?php
         	//$pdo = new PDO('pgsql:host=localhost;port=5432;dbname=GWCourses', 'postgres', '12345678');
         	//echo "done!!!!!!";
-	    	$db = parse_url(getenv("DATABASE_URL"));
+	    	$db = parse_url(getenv("postgres://knrfhlsfnwxwvx:aba906d0136d6b051d50e7e44741c74c72a88f97cc83c46aace2606289c740f1@ec2-174-129-209-212.compute-1.amazonaws.com:5432/d2jcglfroenmhj"));
 
 			$pdo = new PDO("pgsql:" . sprintf(
 			    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-			    $db["host"],
-			    $db["port"],
-			    $db["user"],
-			    $db["pass"],
-			    ltrim($db["path"], "/")
+			    $db["ec2-174-129-209-212.compute-1.amazonaws.com
+"],
+			    $db["5432"],
+			    $db["knrfhlsfnwxwvx"],
+			    $db["aba906d0136d6b051d50e7e44741c74c72a88f97cc83c46aace2606289c740f1"],
+			    ltrim($db["postgres://knrfhlsfnwxwvx:aba906d0136d6b051d50e7e44741c74c72a88f97cc83c46aace2606289c740f1@ec2-174-129-209-212.compute-1.amazonaws.com:5432/d2jcglfroenmhj
+"], "/")
 ));
 
         	$sql = "SELECT studentname, course FROM registercourse";
