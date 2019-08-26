ed18270fe83e245570e405638b97f4a49e4fbfa9
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-27 01:04:39 +0700

    Create catalogue.php

---
+++ catalogue.php
@@ -0,0 +1,15 @@
+ 
+<?php
+    require_once './database.php';
+    //load database of catalogue
+    $query  = "SELECT cId, cName, cDescription from Catalogue";
+    $result  = queryMysql($query );
+    $error1 = $msg1 = "";
+    if (!$result ){
+        $error1 = "Couldn't load data, please try again.";
+    }
+    
+    //load catalogue
+    echo "<div class='side_menu'>";   
+        
+?>
