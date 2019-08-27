4542f28d81b0b801d9228dcb4c5d3ef0014cb8e4
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-27 15:55:21 +0700

    Update catalogue.php

--- catalogue.php
+++ catalogue.php
@@ -1,23 +1,11 @@
  
 <?php
-    include("database.php");
-    $sql = "SELECT cId, cName, cDescription from Catalogue";
-            $stmt = $pdo->prepare($sql);
-            // thiet lap kieudu lieu tra ve
-            $stmt->setFetchMode(PDO::FETCH_ASSOC);
-            $stmt->execute();
-            $resultSet = $stmt->fetchAll();
-    
-    //load catalogue
-    echo "<div class='side_menu'>";   
+    require_once './database.php';
+    $sql = "SELECT cid, cname, cdescription from Catalogue";
+        $stmt = $pdo->prepare($sql);
+        //Thiết lập kiểu dữ liệu trả về
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+        $resultSet = $stmt->fetchAll();
         
 ?>
-<ul>
-        <?php  
-            foreach ($resultSet as $row) {
-            echo '<li>' .
-                $row['cName'] . ' --' . $row['cDescription'] 
-                . '</li>';
-            }
-        ?>
-        </ul>
