35ffebe4ea3ed766d815900f84bb515b90b5543d
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-27 14:39:19 +0700

    Update index-product.php

--- index-product.php
+++ index-product.php
@@ -21,27 +21,24 @@
 
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      
-        // Sử đụng Prepare 
+        
         $stmt = $conn->prepare("SELECT cName, cDescription from Catalogue"); 
          
-        // Thực thi câu truy vấn
+        
         $stmt->execute();
      
-        // Khai báo fetch kiểu mảng kết hợp
+       
         $stmt->setFetchMode(PDO::FETCH_ASSOC); 
          
-        // Lấy danh sách kết quả
+        
         $result = $stmt->fetchAll();
          
-        // Lặp kết quả
+        
         foreach ($result as $item){
             echo $item['cName'] . ' - '. $item['cDescription'];
         }
             
-        catch(PDOException $e) {
-         echo "Lỗi: " . $e->getMessage();
-}
-
+    
 
 
         ?>
