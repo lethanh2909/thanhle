74a54edfa772dd64e51aa491597647a3bf6ee35f
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-28 23:58:00 +0700

    Update addcatalogue.php

--- addcatalogue.php
+++ addcatalogue.php
@@ -10,7 +10,7 @@ require_once './database.php';
         $stmt= $pdo->prepare($sql);
         $stmt->bindParam(':cname', $_POST['cname'], PDO::PARAM_STR);      
         $stmt->bindParam(':cdescription', $_POST['cdescription'], PDO::PARAM_STR);
-        $stmt->execute($data);
+        
 
         
         
