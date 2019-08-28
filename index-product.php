3f71245c3e0bc10bbbc05b31c6e871e409e746c9
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-28 23:43:14 +0700

    Update index-product.php

--- index-product.php
+++ index-product.php
@@ -74,7 +74,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
                                     $ID = $row ['cid'];
                                     $link="?direct=show_product&id=".$ID;        
                                     echo "<a href='$link' class='w3-button w3-large w3-border'>" ;
-                                    $Name = $row ['cname'];                                               
+                                    $Name = $row ['cid'];                                               
                                     echo "$Name";
                                 }
                                  else 
