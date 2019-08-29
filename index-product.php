038bef99165b362a619801f8dc9536df04e82bdc
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-29 14:46:09 +0700

    Update index-product.php

--- index-product.php
+++ index-product.php
@@ -144,7 +144,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
                             echo "</td>";
                             if($i!=3)
                             {
-                                foreach ($resultSet as $row)
+                                foreach ($resultSet as $row);
                             }
                     }
                 }
