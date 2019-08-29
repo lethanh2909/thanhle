1c78e0d5af5e13f042d8c962847ede099eff084c
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-29 10:49:08 +0700

    Update index-product.php

--- index-product.php
+++ index-product.php
@@ -107,47 +107,48 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
         echo "<table>";
             foreach ($resultSet as $row) 
             {    
-                echo "<tr>";
-                    for($i=1;$i<=3;$i++)
+                for($i=1;$i<=3;$i++)
                     {
-                        echo "<td align='center' width='328px' height='228px' >";
-                            if($row!=false)
+                        echo "<tr>";
+                    
+                            echo "<td align='center' width='328px' height='228px' >";
+                                if($row!=false)
+                                {
+                                    $iId = $row['iid'];
+                                    $iName = $row['iname'];
+                                    $iDescription = $row['idescription'];
+                                    $iPrice = $row['iprice'];
+                                    $iStatus = $row['istatus'];
+                                    $iSize = $row['isize'];
+                                    $iImage = $row['iimage'];
+
+                                    $link_image = "./images/item/$iimage";
+                                    $link_detail="?direct=product_detail&id=".$iId;
+
+                                    echo "<a href='$link_detail'>";
+                                        echo "<img src='$link_image' width='200px'>";
+                                    echo "</a>";
+                                    echo "<br>";  
+
+                                    echo "<a href='$link_detail'>";
+                                        echo $iname;
+                                    echo "</a>";
+                                    echo "<br>";  
+
+                                    echo "Price: ".$iprice," $";
+                                    echo "<br>";
+                                }
+                                 else 
+                                {
+                                    echo "&nbsp;";
+                                }
+                            echo "</td>";
+                            if($i!=3)
                             {
-                                $iId = $row['iid'];
-                                $iName = $row['iname'];
-                                $iDescription = $row['idescription'];
-                                $iPrice = $row['iprice'];
-                                $iStatus = $row['istatus'];
-                                $iSize = $row['isize'];
-                                $iImage = $row['iimage'];
-
-                                $link_image = "./images/item/$iimage";
-                                //$link_detail="?direct=product_detail&id=".$iId;
-
-                                echo "<a href='$link_detail'>";
-                                    echo "<img src='$link_image' width='200px'>";
-                                echo "</a>";
-                                echo "<br>";  
-
-                                echo "<a href='$link_detail'>";
-                                    echo $iname;
-                                echo "</a>";
-                                echo "<br>";  
-
-                                echo "Price: ".$iprice," $";
-                                echo "<br>";
+                                $row = $stmt->fetch();
                             }
-                             else 
-                            {
-                                echo "&nbsp;";
-                            }
-                        echo "</td>";
-                        if($i!=3)
-                        {
-                            $row = $stmt->fetch();
-                        }
                     }
-                echo "</tr>";
+                        echo "</tr>";
             }
         echo "</table>";
         ?>        
