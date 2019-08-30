d2c5b8da289b5a0dbb3c5299a31778112629190e
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-30 23:32:46 +0700

    Create deleteitem.php

---
+++ deleteitem.php
@@ -0,0 +1,13 @@
+<?php 
+    include("database.php");
+?>
+<?php	
+	if( isset($_POST['iid']) ){
+    
+    $sql = "DELETE FROM Catalogue WHERE iid = :iid";
+    $stmt= $pdo->prepare($sql);
+    $stmt->bindValue(':iid', $_POST['iid'], PDO::PARAM_INT);
+    $stmt->execute();
+    die("You've deleted the item '$iid' <a href='manageitem.php'>click here</a> to continue.");
+}
+?>
