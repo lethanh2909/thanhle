2d1d4d29930be178ca503ef8732b255d1ee2e973
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-30 23:23:28 +0700

    Update manageitem.php

--- manageitem.php
+++ manageitem.php
@@ -10,6 +10,14 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 5px;
+  text-align: left;    
+}
 </style>
 <body class="w3-light-grey w3-content" style="max-width:1600px">
 
