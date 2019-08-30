15188eb53c0730ee3baa351a6ef86de495e3fdfb
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-30 22:27:52 +0700

    Update updatecatalogue.php

--- updatecatalogue.php
+++ updatecatalogue.php
@@ -2,20 +2,21 @@
     include("database.php");
 ?>
 <?php	
-	if( isset($_POST['cid'], $_POST['cname'])){
-    
-    $sql = "UPDATE Catalogue SET cname = :cname WHERE cid = :cid";
-    $stmt= $pdo->prepare($sql);
-    $stmt->bindValue(':cid', $_POST['cid'], PDO::PARAM_INT);
-    $stmt->bindValue(':cname', $_POST['cname'], PDO::PARAM_STR);
-    $pdoExec = $stmt->execute();
-    
-        // check 
-    if($pdoExec)
-    {
-        echo 'Data updated';
-    }else{
-        echo 'Data Not updated';
-    }
-    
+	if( isset($_POST['cid'], $_POST['cname']))
+	{
+	    
+	    $sql = "UPDATE Catalogue SET cname = :cname WHERE cid = :cid";
+	    $stmt= $pdo->prepare($sql);
+	    $stmt->bindValue(':cid', $_POST['cid'], PDO::PARAM_INT);
+	    $stmt->bindValue(':cname', $_POST['cname'], PDO::PARAM_STR);
+	    $stmt->execute();
+	    
+	        // check 
+	    if($pdoExec)
+	    {
+	        echo 'Data updated';
+	    }else{
+	        echo 'Data Not updated';
+	    }
+	}    
 ?>
