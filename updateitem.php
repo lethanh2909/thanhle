b14da6f7a20bc01e56ecc3a77969b35d6a2ce027
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-31 00:19:07 +0700

    Update updateitem.php

--- updateitem.php
+++ updateitem.php
@@ -23,6 +23,7 @@
 	        echo 'Data Not updated';
 	    }
 	}
+
 ?>
 <br><br>
 <form action="updateitem.php" method="POST">
@@ -31,15 +32,15 @@
         <div class="error"><?php echo $error; ?></div>
         <input type="hidden" value="<?php echo $iid; ?>" name="iid"/>
         Name: </br>
-        <input type="text" id="iname" name="iname" required value="<?php echo $iname; ?>"/><br>
+        <input type="text" id="iname" name="iname" required value="<?php echo $row['iname']; ?>"/><br>
         Description: </br>
-        <input type="text" id="idescription" name="idescription" required value="<?php echo $idescription; ?>"/><br>
+        <input type="text" id="idescription" name="idescription" required value="<?php echo $row['idescription']; ?>"/><br>
         Price: </br>
-        <input type="text"  name="iprice" required value="<?php echo $iprice; ?>"/><br>
+        <input type="text"  name="iprice" required value="<?php echo $row['iprice']; ?>"/><br>
         Status: </br>
-        <input type="text"  name="istatus" required value="<?php echo $istatus; ?>"/><br>
+        <input type="text"  name="istatus" required value="<?php echo $row['istatus']; ?>"/><br>
         Size: </br>
-        <input type="text"  name="isize" required value="<?php echo $isize; ?>"/><br><br>
+        <input type="text"  name="isize" required value="<?php echo $row['isize']; ?>"/><br><br>
         <input type="submit" value="Update"/>
         <div><?php echo $msg; ?></div>
     </fieldset>
