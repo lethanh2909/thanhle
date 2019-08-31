6c4a6e80e5b35dd37a167cd614e4e97fb8a39080
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-31 17:59:12 +0700

    Update managecatalogue.php

--- managecatalogue.php
+++ managecatalogue.php
@@ -132,13 +132,13 @@ th, td {
             echo "<td>$cId</td>";
             
             ?>        
-            <td>
+            
                 <form class="frminline" action="updatecatalogue.php" method="post">
-                    <input type="text" name="cname" required value="<?php echo $cName; ?>" />
+                    <td><input type="text" name="cname" required value="<?php echo $cName; ?>" /></td>
                     <input type="hidden" name="cid" value="<?php echo $row['cid'] ?>" />
                     <td><input type="submit" value="Update" /></td>
                 </form>
-            </td>
+           
             <td>
                 <form class="frminline" action="deletecatalogue.php" method="post" onsubmit="return confirmDelete();">
                     <input type="hidden" name="cid" value="<?php echo $row['cid'] ?>" />
