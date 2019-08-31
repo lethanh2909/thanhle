8cc6d08b7f5ea72335c8d16e67112f4770e3bd0e
le thanh <thanhldgch17337@fpt.edu.vn>
2019-08-31 18:04:55 +0700

    2

--- manageitem.php
+++ manageitem.php
@@ -103,7 +103,7 @@ th, td {
                             <a href="">Manage Items</a>
                             <ul>
                                 <li><a href="additem.php">Add Item</a></li>
-                                <li><a href="manageitem.php">View/Search/Update/Delete item</a></li>
+                                <li><a href="manageitem.php">Update/Delete item</a></li>
                             </ul>
                         </li>
                         <li><a href="index.php">Logout</a></li>
